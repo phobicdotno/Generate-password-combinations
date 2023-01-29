8cb817f69d92a23ff4dd2635c542d23003ca6845
phobicdotno <spamme.post@gmail.com>
2023-01-29 20:13:05 +0100

    Generates file, but does not keep filesize

--- genPass1.c
+++ genPass1.c
@@ -12,47 +12,62 @@ The generateCombinations function uses recursion to generate all possible combin
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include <time.h>
+#include <sys/stat.h>
 
-void generateCombinations(char *prefix, int remaining, int minLength, int maxLength, int MBPerFile) {
-    int i, j, k;
-    if (remaining == 0) {
-        // Print the combination
-        printf("%s\n", prefix);
-        return;
-    }
-    for (i = 32; i <= 126; i++) {
-        char newPrefix[remaining + 1];
-        sprintf(newPrefix, "%s%c", prefix, i);
-        if (remaining == 3) {
-            for (j = '0'; j <= '9'; j++) {
-                for (k = '0'; k <= '9'; k++) {
-                    char newPrefix[remaining + 1];
-                    sprintf(newPrefix, "%s%c%c%c", prefix, i,j,k);
-                    // Print the combination
-                    //printf("%s\n", newPrefix);
-                }
-            }
-        }else{
-            if(remaining >= minLength && remaining <= maxLength){
-                generateCombinations(newPrefix, remaining - 1, minLength, maxLength, MBPerFile);
-            }
-        }
-    }
+#define MAX_COMBINATION_LENGTH 20
+#define MBPerFile 100
+#define MB_TO_BYTES(x) (x * 1024 * 1024)
+#define MAX_FILE_SIZE MB_TO_BYTES(MBPerFile)
+
+void generateCombinations(int length, char *combination, int currentLength, FILE *fp) {
+  if (length == currentLength) {
+    fprintf(fp, "%s\n", combination);
+    return;
+  }
+
+  for (int i = 33; i <= 126; i++) {
+    combination[currentLength] = (char)i;
+    generateCombinations(length, combination, currentLength + 1, fp);
+  }
 }
 
 int main() {
-    int minLength, maxLength, MBPerFile;
-    printf("Enter the minimum length: ");
-    scanf("%d", &minLength);
-    printf("Enter the maximum length: ");
-    scanf("%d", &maxLength);
-    printf("Enter the max file size in MB: ");
-    scanf("%d", &MBPerFile);
-    for(int i=minLength; i<=maxLength; i++) {
-        char prefix[i + 1];
-        memset(prefix, 0, sizeof(prefix));
-        generateCombinations(prefix, i, minLength, maxLength, MBPerFile);
+  int minLength, maxLength;
+  char combination[MAX_COMBINATION_LENGTH + 1];
+  FILE *fp;
+  time_t rawtime;
+  struct tm * timeinfo;
+  char path[100];
+  char filename[100];
+  int fileCounter = 1;
+  int fileSize = 0;
+
+  printf("Enter the minimum length: ");
+  scanf("%d", &minLength);
+
+  printf("Enter the maximum length: ");
+  scanf("%d", &maxLength);
+
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  strftime(path, sizeof(path), "./%Y-%m-%d-%H-%M-%S", timeinfo);
+  mkdir(path, 0700);
+
+  for (int i = minLength; i <= maxLength; i++) {
+    memset(combination, 0, sizeof(combination));
+    sprintf(filename, "%s/combinations-%d.txt", path, fileCounter);
+    fp = fopen(filename, "w");
+    generateCombinations(i, combination, 0, fp);
+    fileSize = ftell(fp);
+    if (fileSize >= MAX_FILE_SIZE) {
+      fileCounter++;
+      fclose(fp);
+      sprintf(filename, "%s/combinations-%d.txt", path, fileCounter);
+      fp = fopen(filename, "w");
+      fileSize = 0;
     }
-    return 0;
+  }
+  fclose(fp);
+  return 0;
 }
