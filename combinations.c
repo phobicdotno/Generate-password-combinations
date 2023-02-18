f6beaa8e3ce358890d09c05a842dfbf3bc75b6e2
phobicdotno <spamme.post@gmail.com>
2023-02-18 20:58:42 +0100

    Compiles and works - still no optional

--- combinations.c
+++ combinations.c
@@ -7,12 +7,17 @@
 #define NUM_CHARS 62  // Number of possible characters
 #define MAX_FILENAME_LEN 100  // Maximum length of filename
 
-void generate_combinations(int min_len, int max_len, int allow_special_chars, int allow_repeats, char *wordlist_file);
-void generate_words(int* num_words, int** word_lengths, char*** words, FILE* wordlist_fp);
-void pad_words(int num_words, int* word_lengths, char** words, int max_word_length);
+// Function prototypes
+//void pad_words(int num_words, int* word_lengths, char** words, int max_word_length);
 int has_repeats(char* combination, int combination_len, int allow_repeats);
 void write_combination(char* combination, int combination_len, int allow_repeats, int allow_special_chars, FILE** fp, char* filename, unsigned long long* file_count, FILE* list_fp);
+void print_progress_and_time(time_t current_time, time_t* last_tick_time, time_t* last_minute_time);
+void print_current_time(time_t current_time);
+void free_memory(int num_words, int* word_lengths, char** words);
+void generate_words(int* num_words, int** word_lengths, char*** words, FILE* wordlist_fp);
+void generate_combinations(int min_len, int max_len, int allow_special_chars, int allow_repeats, char *wordlist_file);
 
+// Main function
 int main() {
     int min_len, max_len, allow_special_chars, allow_repeats;
     char wordlist_file[MAX_FILENAME_LEN];
@@ -30,6 +35,9 @@ int main() {
     return 0;
 }
 
+
+// Generate combinations of characters
+// Generate all combinations of characters
 void generate_combinations(int min_len, int max_len, int allow_special_chars, int allow_repeats, char *wordlist_file) {
     char* filename = (char*) malloc(MAX_FILENAME_LEN);
     char* combination = (char*) malloc(max_len + 1);
@@ -52,6 +60,8 @@ void generate_combinations(int min_len, int max_len, int allow_special_chars, in
             fclose(wordlist_fp);
         }
     }
+
+    // Generate combinations of characters
     for (combination_len = min_len; combination_len <= max_len; combination_len++) {
         unsigned long long num_combinations = pow(allow_special_chars ? (NUM_CHARS + 33) : NUM_CHARS, combination_len);
         for (unsigned long long i = 0; i < num_combinations; i++) {
@@ -76,26 +86,40 @@ void generate_combinations(int min_len, int max_len, int allow_special_chars, in
                 write_combination(combination, combination_len, allow_repeats, allow_special_chars, &fp, filename, &file_count, list_fp);
             }
         }
+        // Print progress and time every 10 seconds, and print current time every minute
         time(&current_time);
-        if (current_time - last_tick_time >= 10) {
-            last_tick_time = current_time;
-            printf(".");
-            fflush(stdout);
-        }
-        if (current_time - last_minute_time >= 60) {
-            last_minute_time = current_time;
-            char* c = ctime(&current_time);
-            printf("\nCurrent time: %s", c);
+        print_progress_and_time(current_time, &last_tick_time, &last_minute_time);
+    }
+
+    // Generate combinations using words from wordlist
+    if (num_words > 0) {
+//        pad_words(num_words, word_lengths, words, max_len);
+        for (int i = min_len; i <= max_len; i++) {
+            unsigned long long num_combinations = pow(num_words, i);
+            for (unsigned long long j = 0; j < num_combinations; j++) {
+                memset(combination, 0, max_len + 1);
+                unsigned long long k = j;
+                int l;
+                int index;
+            for (l = 0; l < i; l++) {
+                if (l == 0) {
+                    index = k % num_words;
+                } else {
+                    k /= num_words;
+                    index = k % num_words;
+                }
+                strcat(combination, words[index]);
+            }
+            int has_repeating_chars = has_repeats(combination, i, allow_repeats);
+            if (!has_repeating_chars) {
+                write_combination(combination, i, allow_repeats, allow_special_chars, &fp, filename, &file_count, list_fp);
+                }
+            }
         }
     }
-    if (fp != NULL) {
-        fclose(fp);
-    }
-    fclose(list_fp);
-    free(combination);
-    free(filename);
 }
 
+// Check if a combination of characters has repeating characters
 int has_repeats(char* combination, int combination_len, int allow_repeats) {
     if (allow_repeats) {
         return 0;
@@ -109,15 +133,19 @@ int has_repeats(char* combination, int combination_len, int allow_repeats) {
     return 0;
 }
 
+// Write combination of characters to file
 void write_combination(char* combination, int combination_len, int allow_repeats, int allow_special_chars, FILE** fp, char* filename, unsigned long long* file_count, FILE* list_fp) {
     if (!has_repeats(combination, combination_len, allow_repeats)) {
+        // Create new file if necessary
         if (*fp == NULL) {
             snprintf(filename, MAX_FILENAME_LEN, "generated%04llu.txt", *file_count);
             *fp = fopen(filename, "w");
             printf("Creating file: %s\n", filename);
             fprintf(list_fp, "%s %s\n", combination, filename);
         }
+        // Write combination to file
         fprintf(*fp, "%s\n", combination);
+        // Close file and create new one if it's too big
         if (ftell(*fp) >= 1000000000) {  // 1 GB
             fclose(*fp);
             *fp = NULL;
@@ -126,8 +154,34 @@ void write_combination(char* combination, int combination_len, int allow_repeats
     }
 }
 
+// Print progress and time every 10 seconds, and print current time every minute
+void print_progress_and_time(time_t current_time, time_t* last_tick_time, time_t* last_minute_time) {
+    if (current_time - *last_tick_time >= 10) {
+        *last_tick_time = current_time;
+        printf(".");
+        fflush(stdout);
+    }
+    if (current_time - *last_minute_time >= 60) {
+        *last_minute_time = current_time;
+        print_current_time(current_time);
+    }
+}
 
+// Print current time
+void print_current_time(time_t current_time) {
+    char* c = ctime(&current_time);
+    printf("\nCurrent time: %s", c);
+}
 
+// Free memory allocated for wordlist
+void free_memory(int num_words, int* word_lengths, char** words) {
+    int i;
+    for (i = 0; i < num_words; i++) {
+        free(words[i]);
+    }
+    free(word_lengths);
+    free(words);
+}
 void generate_words(int* num_words, int** word_lengths, char*** words, FILE* wordlist_fp) {
     int i, j;
     char line[256];
@@ -143,17 +197,6 @@ void generate_words(int* num_words, int** word_lengths, char*** words, FILE* wor
             *words = (char**) realloc(*words, (*num_words) * sizeof(char*));
             (*words)[(*num_words) - 1] = (char*) malloc(word_length + 1);
             strcpy((*words)[(*num_words) - 1], line);
-        }
-    }
-}
-
-void pad_words(int num_words, int* word_lengths, char** words, int max_word_length) {
-    int i, j;
-    // Pad shorter words with spaces
-    for (i = 0; i < num_words; i++) {
-        int pad_length = max_word_length - word_lengths[i];
-        for (j = 0; j < pad_length; j++) {
-            words[i][word_lengths[i] + j] = ' ';
         }
     }
 }
